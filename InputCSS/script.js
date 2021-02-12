75dae1b51c475f4f74db6e6230aae714b407e98d
iannakim <dear.hyunji@gmail.com>
2021-02-11 21:37:17 -0500

    Button hover complete

--- InputCSS/script.js
+++
@@ -1,9 +0,0 @@
-const email = document.querySelector("input#nemail")
-
-
-
-email.addEventListener('change', (evt) => {
-  if (evt.target.value.includes("@")){
-    email.style.color = 'green'
-  }
-})
